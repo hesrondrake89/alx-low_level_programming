b9d043f9dad06fc9ec83d224ff465adcad5b81a9
hesrondrake89 <113859517+hesrondrake89@users.noreply.github.com>
2022-11-14 17:32:36 +0000

    task4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,11 +1,59 @@
 #include "dog.h"
-
 #include <stdlib.h>
 
+/**
+ * new_dog -  creates a new dog.
+ *
+ *
+ * @name: char type
+ *
+ * @age: float type
+ *
+ * @owner: char type
+ *
+ * Return: Pointer to struct
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *newDog;
+	int length1, length2, i, j;
+
+	length1 = 0, length2 = 0;
+	newDog = malloc(sizeof(struct dog));
+
+	if (newDog == NULL)
+		return (NULL);
+
+	while (name[length1++])
+		;
+	while (owner[length2++])
+		;
+
+	newDog->name = malloc(sizeof(newDog->name) * length1);
+
+	if (newDog->name == NULL)
+	{
+		free(newDog);
+		return (NULL);
+	}
+
+	for (i = 0; i < length1; i++)
+		newDog->name[i] = name[i];
+
+	newDog->age = age;
 
 
-int _strlen(char *str);
+	newDog->owner = malloc(sizeof(newDog->owner) * length2);
 
-char *_strcopy(char *dest, char *src);
+	if (newDog->owner == NULL)
+	{
+		free(newDog->name);
+		free(newDog);
+		return (NULL);
+	}
+	for (j = 0; j < length2; j++)
+		newDog->owner[j] = owner[j];
 
-dog_t *new_dog(char *name, float age, char *owner);
+	return (newDog);
+}
