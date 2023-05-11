cf78d7d768c9f5c8d42d46055cb3d332e18d3724
hesrondrake89 <113859517+hesrondrake89@users.noreply.github.com>
2023-05-11 13:02:57 +0000

    tasks

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -1,3 +1,8 @@
+/*
+ * File: 0-linear.c
+ * Auth: Hesron Atee
+ */
+
 #include "search_algos.h"
 
 /**
@@ -9,6 +14,7 @@
  * @value: value to search in
  * Return: index of the number
  */
+
 int interpolation_search(int *array, size_t size, int value)
 {
 	size_t pos, low, high;
